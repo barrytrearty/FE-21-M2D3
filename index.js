d0a33c170f5c8939ad5c525c8653277915f2df7e
barrytrearty <btrearty@gmail.com>
2021-07-07 15:45:16 +0100

    all exercises complete. tq mridula

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 "strict mode";
+
+const images = document.getElementsByTagName("img");
+const cards = document.querySelectorAll("div .card");
+const cardsBody = document.querySelectorAll("#last-minute .card");
+const summerDeals = document.querySelectorAll(".summer-deals");
+
+console.log(cards);
+
+const countTravels = function () {
+  return images.length;
+};
+
+const deleteCards = function () {
+  for (i = 0; i < cards.length; i++) {
+    cards[i].remove();
+    cardsBody[i].remove();
+    return;
+  }
+};
+
+const addHot = function () {
+  for (j = 0; j < summerDeals.length; j++) {
+    let badge = document.createElement("span");
+    badge.innerText = "HOT";
+    badge.classList.add("badge", "badge-danger");
+    summerDeals[j].appendChild(badge);
+  }
+};
+
+addHot();
+
+// deleteCards();
+
+// windows.onload(countTravels())
+
+console.log(countTravels());
